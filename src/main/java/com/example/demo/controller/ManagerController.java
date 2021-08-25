b949043dcb5155763a7ab95b59f27d8530c4c4e9
nok3zy <nok3zy@gmail.com>
2021-08-25 16:39:16 +0900

    feat : Add Query with pagination - file_apply_form .

--- src/main/java/com/example/demo/controller/ManagerController.java
+++ src/main/java/com/example/demo/controller/ManagerController.java
@@ -1,9 +1,15 @@
 package com.example.demo.controller;
 
 import com.example.demo.dao.MemberDao;
+import com.example.demo.domain.entity.ApplyFileNoticeEntity;
+import com.example.demo.domain.entity.FileNotice;
 import com.example.demo.domain.entity.NoticeEntity;
+import com.example.demo.domain.entity.StatusName;
+import com.example.demo.domain.repository.ApplyFileRepository;
 import com.example.demo.domain.repository.MemberRepository;
 import com.example.demo.domain.repository.NoticeRepository;
+import com.example.demo.dto.FileApplyDto;
+import com.example.demo.dto.FileNoticeDto;
 import com.example.demo.dto.NoticeInfoDto;
 import com.example.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +24,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +34,8 @@ import static java.util.stream.Collectors.toList;
 @Slf4j
 @Controller
 @RequiredArgsConstructor
+@Secured({ "ROLE_ADMIN" })
+@ResponseBody
 @RequestMapping("/manage")
 public class ManagerController {
 
@@ -34,10 +44,11 @@ public class ManagerController {
     @Autowired
     private final MemberRepository memberRepository;
     @Autowired
+    private  final ApplyFileRepository applyFileRepository;
+    @Autowired
     private final NoticeRepository noticeRepository;
 
-    @Secured({ "ROLE_ADMIN" })
-    @ResponseBody
+
     @GetMapping()//현재 사용자 정보 받아오기
     public List<NoticeEntity> getManager(HttpServletRequest request){
 
@@ -49,7 +60,7 @@ public class ManagerController {
         return noticeEntitys;
     }
 
-    @ResponseBody
+
     @GetMapping("/{pageNum}")
     public Page<NoticeInfoDto> getManagerPage(HttpServletRequest req, @PathVariable int pageNum) {
         Pageable page = PageRequest.of(pageNum, 5, Sort.by("uploadDay").descending());
@@ -61,4 +72,19 @@ public class ManagerController {
 
         return new PageImpl<>(noticeInfoDtoList, page, noticeEntityPages.getTotalElements());
     }
+
+
+    @GetMapping("/{noticeId}/{pageNum}")
+    public Page<FileApplyDto> getNotice(@PathVariable Long noticeId,@PathVariable int pageNum) {
+        Pageable page = PageRequest.of(pageNum, 5, Sort.by("member_id").ascending());
+
+        Page<ApplyFileNoticeEntity> applyPages = applyFileRepository.findMemberById(noticeId,page);
+        List<FileApplyDto> fileApplyDtoList = applyPages.stream().map(a-> new FileApplyDto(a) ).collect((toList()));
+
+
+        return new PageImpl<>(fileApplyDtoList, page, applyPages.getTotalElements());
+    }
+
+
+
 }
--- src/main/java/com/example/demo/domain/repository/ApplyFileRepository.java
+++ src/main/java/com/example/demo/domain/repository/ApplyFileRepository.java
@@ -1,7 +1,21 @@
 package com.example.demo.domain.repository;
 
+import com.example.demo.dao.MemberDao;
 import com.example.demo.domain.entity.ApplyFileNoticeEntity;
+import com.example.demo.domain.entity.FileNotice;
+import com.example.demo.domain.entity.NoticeEntity;
+import com.example.demo.dto.FileNoticeDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ApplyFileRepository extends JpaRepository<ApplyFileNoticeEntity, Long> {
+
+    @Query(value = "select * from apply_file n where n.notice_id = ?1",
+            countQuery = "SELECT COUNT(*) FROM apply_file n where n.notice_id=?1",
+            nativeQuery = true)
+    Page<ApplyFileNoticeEntity> findMemberById(Long noticeId,Pageable pageable);
 }
